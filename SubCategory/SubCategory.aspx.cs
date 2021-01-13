c3ccfcb42b93e6ea32fe3da08056c1d444e9a896
Vidhi Doshi <development.ves@gmail.com>
2021-01-13 14:52:03 +0530

    sosho admin

--- SubCategory/SubCategory.aspx.cs
+++ SubCategory/SubCategory.aspx.cs
@@ -16,8 +16,19 @@ public partial class SubCategory_SubCategory : System.Web.UI.Page
         {
             try
             {
-                txtdt.Text = DateTime.Now.ToString("dd/MM/yyyy");
-                txtdt1.Text = DateTime.Now.ToString("dd/MM/yyyy");
+                string categoryqry = "SELECT CategoryId,CategoryName FROM Category where isnull(IsDeleted,0)=0 AND isnull(IsActive,0)=1 order by Sequence asc";
+                DataTable dtcategory = new DataTable();
+                dtcategory = dbc.GetDataTable(categoryqry);
+                dtcategory.Rows.Add("0", "Select Category");
+                DataView dv = dtcategory.DefaultView;
+                dv.Sort = "CategoryId asc";
+                DataTable sortedDT = dv.ToTable();
+                ddlCategoryName.DataSource = sortedDT;
+                ddlCategoryName.DataTextField = "CategoryName";
+                ddlCategoryName.DataValueField = "CategoryId";
+                ddlCategoryName.DataBind();
+                //txtdt.Text = DateTime.Now.ToString("dd/MM/yyyy");
+                //txtdt1.Text = DateTime.Now.ToString("dd/MM/yyyy");
                 DataList();
             }
             catch (Exception W) { }
@@ -26,18 +37,32 @@ public partial class SubCategory_SubCategory : System.Web.UI.Page
     }
     private void DataList()
     {
-        String from = txtdt.Text.ToString();
-        String to = txtdt1.Text.ToString();
+        //String from = txtdt.Text.ToString();
+        //String to = txtdt1.Text.ToString();
 
-        String[] StrPart = from.Split('/');
+        //String[] StrPart = from.Split('/');
 
-        String[] StrPart1 = to.Split('/');
+        //String[] StrPart1 = to.Split('/');
+        int isActive = 0;
+        if (chkisactive.Checked)
+        {
+            isActive = 1;
+        }
 
-        string query = "SELECT S.Id,S.SubCategory,S.CategoryID,C.CategoryName,S.Description,S.IsActive,S.CreatedOn " + 
-                       " FROM tblSubCategory S " + 
+        int categoryId = Convert.ToInt32(ddlCategoryName.SelectedValue);
+        string query = "SELECT S.Id,S.SubCategory,S.CategoryID,C.CategoryName,S.Description,S.IsActive,S.CreatedOn " +
+                       " FROM tblSubCategory S " +
                        " LEFT JOIN Category C ON C.CategoryID = S.CategoryId " +
-                       " where isnull(S.IsDeleted,0)=0  and convert(date,S.CreatedOn,103)>='" + StrPart[2] + "-" + StrPart[1] + "-" + StrPart[0] + "' and convert(date,S.CreatedOn,103)<='" + StrPart1[2] + "-" + StrPart1[1] + "-" + StrPart1[0] + "'  order by S.CreatedOn desc ";
+                       " where isnull(S.IsDeleted,0)=0  " +
+                        " AND ISNULL(S.ISActive, 0) = " + isActive;
 
+
+        if (categoryId > 0)
+        {
+            query += " and S.CategoryID=" + categoryId;
+        }
+        //"and convert(date,S.CreatedOn,103)>='" + StrPart[2] + "-" + StrPart[1] + "-" + StrPart[0] + "' and convert(date,S.CreatedOn,103)<='" + StrPart1[2] + "-" + StrPart1[1] + "-" + StrPart1[0] + 
+        query += "  order by S.sequence desc ";
         DataTable dtSubCategorylist = dbc.GetDataTable(query);
         if (dtSubCategorylist.Rows.Count > 0)
         {
@@ -57,4 +82,10 @@ public partial class SubCategory_SubCategory : System.Web.UI.Page
             e.Row.TableSection = TableRowSection.TableHeader;
         }
     }
+
+    protected void OnSelectedIndexChanged(object sender, EventArgs e)
+    {
+        DataList();
+    }
+
 }
